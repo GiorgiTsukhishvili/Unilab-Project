100f1f2a2d39c6fb2ba37883fdd4c6ab3c4b8665
Giorgi Tsukhishvili <104715456+GiorgiTsukhishvili@users.noreply.github.com>
2022-06-18 01:08:57 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const movingMenu = document.querySelector(".mobile-menu");
 menu.addEventListener("click", function () {
   movingMenu.classList.add("active-menu");
 });
+
+const overlay = document.querySelector(".overlay");
+
+overlay.addEventListener("click", function () {
+  movingMenu.classList.remove("active-menu");
+  overlay.classList.add("hidden");
+});
